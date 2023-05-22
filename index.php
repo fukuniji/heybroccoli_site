8a43b05a7af0ec0b655e80eba4c77b215ebc1a46
Evgenia0712 <e.gunko1992@mail.ru>
2023-05-23 02:36:11 +0300

    feat: сделала HTML-скелет footer

--- index.php
+++ index.php
@@ -502,14 +502,20 @@
       <div class="footer__text">@nutrition2023</div>
       <nav class="footer__nav nav">
         <h6>Меню</h6>
-        <a href="#about">Обо мне</a>
-        <a href="#approach">Подход к работе</a>
-        <a href="#task">Чем могу помочь</a>
-        <a href="#services">Услуги</a>
-        <a href="#rewiew">Отзывы</a>
+        <a href="#about" class="nav__link">Обо мне</a>
+        <a href="#approach" class="nav__link">Подход к работе</a>
+        <a href="#task" class="nav__link">Чем могу помочь</a>
+        <a href="#services" class="nav__link">Услуги</a>
+        <a href="#rewiew" class="nav__link">Отзывы</a>
       </nav>
+      <div class="footer__socials">
+        <h6>Подписывайтесь</h6>
+        <a href="#" class="footer__link">Instagram</a>
+          <!-- ADD INSTAGRAM LINK -->
+        <a href="https://t.me/heybroccoli" class="footer__link">Мой блог об изменении привычек "А как же брокколи"</a>
+        <a href="https://t.me/roadinside_me" class="footer__link">Заметки об эмиграции и приключениях "Взяла и переехала"</a>
+      </div>
       <div></div>
-    
     </div>
   </footer>
     <!-- ---------------------FOOTER END--------------------------->
