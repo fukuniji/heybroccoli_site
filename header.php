45b505f2a614fd269c1d79ca7c0f8b546091f592
Lanasites <mochegova@gmail.com>
2023-05-25 00:35:19 +0300

    feat: add header

--- header.php
+++ header.php
@@ -1,30 +1,25 @@
 <!-- ---------------------HEADER--------------------------->
 
-  <header class="header">
-    <div class="container">
-      <a href="#"><img src="./assets/image/footer/logo.svg" alt="logo" class="header__logo"></a>
-
-      <ul class="header__nav nav">
-        <li class="nav__item nav__item_activ">
-          <a href="#about" class="nav__link"> Обо мне </a>
-        </li>
-        <li class="nav__item nav__item_activ">
-          <a href="#approach" class="nav__link"> Подход к работе</a>
-        </li>
-        <li class="nav__item nav__item_activ">
-          <a href="#task" class="nav__link"> Чем могу помочь </a>
-        </li>
-        <li class="nav__item nav__item_activ">
-          <a href="#services" class="nav__link"> Услуги </a>
-        </li>
-        <li class="nav__item nav__item_activ">
-          <a href="#rewiew" class="nav__link"> Отзывы </a>
-        </li>
-        <li class="nav__item nav__item_activ">
-          <a href="#sertificates" class="nav__link"> Сертификаты</a>
-        </li>
-      </ul>
+<header class="header" id="header">
+  <div class="container" id="header__conteiner">
+    <!-- <a href="#"><img src="./assets/image/footer/logo.svg" alt="logo" class="header__logo"></a> -->
+    <nav class="header__nav nav" id="header__menu">
+      <a href="#about" class="nav__link"> Обо мне </a>
+      <a href="#approach" class="nav__link"> Подход к работе</a>
+      <a href="#task" class="nav__link"> Чем могу помочь </a>
+      <a href="#services" class="nav__link"> Услуги </a>
+      <a href="#rewiew" class="nav__link"> Отзывы </a>
+      <a href="#sertificates" class="nav__link"> Сертификаты</a>
+    </nav>
+  </div>
+  <!-- для гамбургера -->
+  <div class="hamb">
+    <div class="hamb__field" id="hamb">
+      <span class="bar"></span> <span class="bar"></span>
+      <span class="bar"></span>
     </div>
-  </header>
+  </div>
+  <div class="popup" id="popup"></div>
+</header>
 
 <!-- ---------------------HEADER END--------------------------->
