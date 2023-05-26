5ee111ad8082ef95c3a78715a7bb8d7bbdec6d91
Evgenia0712 <e.gunko1992@mail.ru>
2023-05-27 02:32:25 +0300

    feat: add desktop scss

--- services-business.php
+++ services-business.php
@@ -2,13 +2,17 @@
 
   <div class="main__services-business services-business">
     <div class="container">
-      <h2 class="services-business__title title">Услуги для бизнеса </h2>
 
+      <div class="services-business__business">
+        <h2 class="services-business__title title">Услуги для бизнеса </h2>
+        <div class="services-business__info-wrapper">
+          <img class="services-business__img-1" src="./assets/image/services-business/2people.png" alt="Елена с коллегой">
+          <div class="services-business__text-1"> Если вы <span class="bluetext"> подрядчик</span>, который способен организовать вкусный и сбалансированный <span class="bluetext "> кейтеринг</span>, тоже напишите мне, найдем возможности для сотрудничества.</div>
+          <div class="services-business__text-2">Если вы хотите придумать классные и полезные закуски на <span class="bluetext ">вечеринку, детский праздник, пресс-конференцию</span>, танцевальный <span class="bluetext ">фестиваль</span> или провести <span class="bluetext ">лекцию</span> для своих сотрудников по теме питания, подобрать меню для столовой или ретрита — я могу вам помочь.</div>
+          <img class="services-business__img-2" src="./assets/image/services-business/4people.png" alt="Елена с коллегами">
+        </div>
+      </div>
 
-      <img src="./assets/image/services-business/2people.png" alt="Елена с коллегой" class="services-business__img-1">
-      <div class="services-business__text-1"> Если вы <span class="bluetext"> подрядчик</span>, который способен организовать вкусный и сбалансированный <span class="bluetext "> кейтеринг</span>, тоже напишите мне, найдем возможности для сотрудничества.</div>
-      <div class="services-business__text-2">Если вы хотите придумать классные и полезные закуски на <span class="bluetext ">вечеринку, детский праздник, пресс-конференцию</span>, танцевальный <span class="bluetext ">фестиваль</span> или провести <span class="bluetext ">лекцию</span> для своих сотрудников по теме питания, подобрать меню для столовой или ретрита — я могу вам помочь.</div>
-      <img src="./assets/image/services-business/4people.png" alt="Елена с коллегами" class="services-business__img-2">
     </div>
   </div>
 
