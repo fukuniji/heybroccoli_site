fd1bd21f902ec447705208ff6afa49c4e3675927
Lanasites <mochegova@gmail.com>
2023-05-28 01:01:11 +0300

    feat: style: добавление стилизации футера

--- footer.php
+++ footer.php
@@ -1,24 +1,29 @@
   <!-- ---------------------FOOTER--------------------------->
   <footer class="footer">
     <div class="container">
-      <img class="footer__logo logo" src="./assets/image/footer/logo.svg" alt="логотип">
-      <div class="footer__text">@nutrition2023</div>
-      <nav class="footer__nav nav">
-        <h6>Меню</h6>
-        <a href="#about" class="nav__link">Обо мне</a>
-        <a href="#approach" class="nav__link">Подход к работе</a>
-        <a href="#task" class="nav__link">Чем могу помочь</a>
-        <a href="#services" class="nav__link">Услуги</a>
-        <a href="#rewiew" class="nav__link">Отзывы</a>
-      </nav>
-      <div class="footer__socials">
-        <h6>Подписывайтесь</h6>
-        <a href="#" class="footer__link">Instagram</a>
-        <!-- ADD INSTAGRAM LINK -->
-        <a href="https://t.me/heybroccoli" class="footer__link">Мой блог об изменении привычек "А как же брокколи"</a>
-        <a href="https://t.me/roadinside_me" class="footer__link">Заметки об эмиграции и приключениях "Взяла и переехала"</a>
+      <div class="footer__part-logo">
+        <img class="footer__logo logo" src="./assets/image/footer/logo.svg" alt="логотип">
+        <div class="footer__text">@nutrition2023</div>
       </div>
+      <div class="footer__part-nav">
+        <nav class="footer__nav nav">
+          <h5>Меню</h5>
+          <a href="#about" class="nav__link">Обо мне</a>
+          <a href="#approach" class="nav__link">Подход к работе</a>
+          <a href="#task" class="nav__link">Чем могу помочь</a>
+          <a href="#services" class="nav__link">Услуги</a>
+          <a href="#rewiew" class="nav__link">Отзывы</a>
+        </nav>
+        <div class="footer__socials">
+          <h5>Подписывайтесь</h5>
+          <a href="https://instagram.com/elena_bonvoyage" class="footer__link">Instagram</a>
+          <!-- ADD INSTAGRAM LINK -->
+          <a href="https://t.me/heybroccoli" class="footer__link">Мой блог об изменении привычек "А как же брокколи"</a>
+          <a href="https://t.me/roadinside_me" class="footer__link">Заметки об эмиграции и приключениях "Взяла и переехала"</a>
+        </div>
+      </div>
+      <div class="footer__text-mobile">@nutrition2023</div>
     </div>
   </footer>
-  
+
   <!-- ---------------------FOOTER END--------------------------->
