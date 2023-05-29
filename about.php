9c7e1afc0e8dea730aa133dfd500dbbee98b7b12
Diana0sip <diana.dk0sipova@gmail.com>
2023-05-29 03:50:08 +0300

    сделала верстку и адаптив

--- about.php
+++ about.php
@@ -4,14 +4,19 @@
 
   <div class="main__about about" id="about">
     <div class="about__wrapper">
+
       <img src="./assets/image/about/ElenaKulikova.jpg" alt="Куликова Елена" class="about__photo" >
       <div class="about__section section">
-        <h3 class="section__slogan slogan">Привычки, а не диеты!</h3>
-        <div class="section__img group-img">
-          <img src="./assets/image/about/line.png" alt="" class="line">
-          <img src="./assets/image/logo_green.svg" alt="" class="group-img__img">
+        
+      <div class="section__group-divlogo grouplogo">
+          <h3 class="grouplogo__slogan slogan">Привычки, а не диеты!</h3>
+          
+          <div class="grouplogo__divider"></div>
+          <!--<img src="./assets/image/about/line.png" alt="" class="line">-->
+          <img src="./assets/image/logo_green.svg" alt="" class="grouplogo__logo">
         </div>
-        <h2 class="section__title">Куликова Елена</h2>
+
+        <h1 class="section__title">Куликова Елена</h1>
         <div class="section__subtitle">Нутрициолог, коуч полезных привычек, change maker</div>
       </div>
     </div>
