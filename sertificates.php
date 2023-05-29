2b89c438cdab1b8424c2b5dd437dc837c64d92c4
Yanastya89 <belii.nasteniw@gmail.com>
2023-05-29 23:18:02 +0900

    feat: block-10 адаптив

--- sertificates.php
+++ sertificates.php
@@ -4,27 +4,24 @@
     <div class="container">
       <h2 class="sertificates__title title"> Мои сертификаты </h2>
       <div class="sertificates__block block ">
-        <div class="block__row-1 row-1">
-          <div class="row-1__text">
-            <span class="bluetext ">Основа моего подхода</span> — знания и навыки, полученные в канадской Precision Nutrition. Главная особенность PN — сочетание научных знаний и индивидуального подхода. Этим нутрициолог отличается от диетолога: второй говорит, что нужно делать. Первый — что и как.
-          </div>
-          <div>
-            <img src="./assets/image/sertificates/sertificate-3.jpg" alt="" class="row-1__photo">
-          </div>
-          <div>
-            <img src="./assets/image/sertificates/sertificate-4.jpg" alt="" class="row-1__photo">
-          </div>
+        <div class="block__text_1">
+          <span class="bluetext ">Основа моего подхода</span> — знания и навыки, полученные в канадской Precision Nutrition. Главная особенность PN — сочетание научных знаний и индивидуального подхода. Этим нутрициолог отличается от диетолога: второй говорит, что нужно делать. Первый — что и как.
         </div>
-        <div class="block__row-2 row-2">
-          <div>
-            <img src="./assets/image/sertificates/sertificate-1.jpg" alt="" class="row-2__photo">
-          </div>
-          <div>
-            <img src="./assets/image/sertificates/sertificate-2.jpg" alt="" class="row-2__photo">
-          </div>
+        <div class="block__photo_1">
+          <img src="./assets/image/sertificates/sertificate-1.jpg" alt="sertificate">
+        </div>
+        <div class="block__text_2">
+          <span class="bluetext ">Основа моего подхода</span> — знания и навыки, полученные в канадской Precision Nutrition. Главная особенность PN — сочетание научных знаний и индивидуального подхода. Этим нутрициолог отличается от диетолога: второй говорит, что нужно делать. Первый — что и как.
+        </div>
+        <div class="block__photo_2">
+          <img src="./assets/image/sertificates/sertificate-1.jpg" alt="sertificate">
+        </div>
+        <div class="block__photo_3">
+          <img src="./assets/image/sertificates/sertificate-3.jpg" alt="sertificate">
         </div>
       </div>
     </div>
   </div>
+
 
   <!-- ---------------------SERTIFICATES-10 END--------------------------->
