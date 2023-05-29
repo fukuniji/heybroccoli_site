ad4ccf93b1749aa4223890d04c9d4705b96fb56d
fukuniji <marusina.nat@gmail.com>
2023-05-29 12:03:54 +0500

    feat: add images in block 11

--- message-me.php
+++ message-me.php
@@ -35,7 +35,21 @@
 
     </div>
     <!-- <div class="message-me__photo"></div> -->
-    <img src="./assets/image//form/elena.jpg" alt="Елена" class="message-me__img">
+    <!-- <img src="./assets/image//form/elena.jpg" alt="Елена" class="message-me__img"> -->
+    <picture>
+      <!-- retina: -->
+      <!-- <source media="(min-resolution: 2dppx)" srcset="./assets/image/form/elena-photo_11_desktop_x2.png"> -->
+      <!-- <source media="(min-width: 1441px)" srcset="./assets/image/form/elena-photo_11_desktop_x2.png"> -->
+      <!-- desktop: -->
+      <source media="(min-width: 1024px)" srcset="./assets/image/form/elena-photo_11_desktop.png">
+      <!-- tablet: -->
+      <source media="(min-width: 768px)" srcset="./assets/image/form/elena-photo_11_tablet.png">
+      <!-- mobile: -->
+      <source media="(max-width: 767px)" srcset="./assets/image/form/elena-photo_11_mobile.png">
+      <!-- если не поддерживается source -->
+      <img src="image/elena-photo_11_desktop.png" width="700" height="783" alt="Нутрициолог Куликова Елена">
+      <!-- если не поддерживается source - доделать -->
+    </picture>
   </div>
   <!-- ---------------------FORM-11 END------------------------- -->
 
