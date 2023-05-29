2511f5fb2607df986b97c6ded44d7dfb3e900d3f
Yanastya89 <belii.nasteniw@gmail.com>
2023-05-29 17:48:52 +0900

    feat: block sertificates desktop

--- sertificates.php
+++ sertificates.php
@@ -1,20 +1,28 @@
   <!-- ---------------------SERTIFICATES-10-------------------------->
 
   <div class="main__sertificates sertificates" id="sertificates">
-    <h2 class="sertificates__title title"> Мои сертификаты </h2>
-    <div class="sertificates__text">
-      <span class="bluetext ">Основа моего подхода</span> — знания и навыки, полученные в канадской Precision Nutrition. На данный момент я заканчиваю обучение, но у меня уже есть право на практику. Здесь обучают коучей по питанию с 2005 года, это диплом международного образца, признанный во всех странах. Главная особенность PN — сочетание научных знаний и индивидуального подхода. Этим нутрициолог отличается от диетолога: второй говорит, что нужно делать. Первый — что и как.
-      <div class="sertificates__photo">
-        <img src="#" alt="">
-      </div>
-      <div class="sertificates__photo">
-        <img src="#" alt="">
-      </div>
-      <div class="sertificates__photo">
-        <img src="#" alt="">
-      </div>
-      <div class="sertificates__photo">
-        <img src="#" alt="">
+    <div class="container">
+      <h2 class="sertificates__title title"> Мои сертификаты </h2>
+      <div class="sertificates__block block ">
+        <div class="block__row-1 row-1">
+          <div class="row-1__text">
+            <span class="bluetext ">Основа моего подхода</span> — знания и навыки, полученные в канадской Precision Nutrition. Главная особенность PN — сочетание научных знаний и индивидуального подхода. Этим нутрициолог отличается от диетолога: второй говорит, что нужно делать. Первый — что и как.
+          </div>
+          <div>
+            <img src="./assets/image/sertificates/sertificate-3.jpg" alt="" class="row-1__photo">
+          </div>
+          <div>
+            <img src="./assets/image/sertificates/sertificate-4.jpg" alt="" class="row-1__photo">
+          </div>
+        </div>
+        <div class="block__row-2 row-2">
+          <div>
+            <img src="./assets/image/sertificates/sertificate-1.jpg" alt="" class="row-2__photo">
+          </div>
+          <div>
+            <img src="./assets/image/sertificates/sertificate-2.jpg" alt="" class="row-2__photo">
+          </div>
+        </div>
       </div>
     </div>
   </div>
