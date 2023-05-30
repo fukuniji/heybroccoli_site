6f51e63cea9c2ac0a7f69e5e9e6f674be95c31bd
Lanasites <mochegova@gmail.com>
2023-05-30 23:15:01 +0300

    fix: правки по слайдеру отзывов

--- reviews.php
+++ reviews.php
@@ -6,42 +6,50 @@
         <h2 class="reviews__title">Отзывы</h2>
         <div class="reviews__slider">
           <div class="reviews__slider__button-left">
-            <img src="./assets/image/reviews/arrow.svg" alt="arrow_previous" id="prev" />
+            <!-- <img src="./assets/image/reviews/arrow.svg" alt="arrow_previous" id="prev" /> -->
+            <svg width="40" height="40" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" alt="arrow_previous" id="prev" class="arrow">
+              <circle cx="15" cy="15" r="14.1" stroke="#557DB0" stroke-width="1.8" />
+              <path d="M20.5 8L7 14.5L20.5 22.5" stroke="#557DB0" stroke-width="1.8" />
+            </svg>
           </div>
           <div class="reviews__slider__pictures">
             <div class="reviews__slider__slider">
               <div class="reviews__slider__slide">
-                <img src="./assets/image/reviews/Maria_Rusia1.jpg" alt="отзывы" />
+                <img src="./assets/image/reviews/Maria_Rusia1.jpg" alt="отзывы" class="vertikal" />
                 <p class="reviewer">Мария, дизайнер интерьеров, Россия</p>
               </div>
               <div class="reviews__slider__slide">
-                <img src="./assets/image/reviews/Maria_Rusia2.jpeg" alt="отзывы" />
+                <img src="./assets/image/reviews/Maria_Rusia2.jpeg" alt="отзывы" class="vertikal" />
                 <p class="reviewer">Мария, дизайнер интерьеров, Россия</p>
               </div>
               <div class="reviews__slider__slide">
-                <img src="./assets/image/reviews/Maria_Rusia3.jpeg" alt="отзывы" />
+                <img src="./assets/image/reviews/Maria_Rusia3.jpeg" alt="отзывы" class="vertikal" />
                 <p class="reviewer">Мария, дизайнер интерьеров, Россия</p>
               </div>
               <div class="reviews__slider__slide">
-                <img src="./assets/image/reviews/Maria_Gr.jpeg" alt="отзывы" />
+                <img src="./assets/image/reviews/Maria_Gr.jpeg" alt="отзывы" class="vertikal" />
                 <p class="reviewer">Мария, журналистка, Грузия</p>
               </div>
               <div class="reviews__slider__slide">
-                <img src="./assets/image/reviews/review_yana_fr.jpg" alt="отзывы" />
+                <img src="./assets/image/reviews/review_yana_fr.jpg" alt="отзывы" class="vertikal" />
                 <p class="reviewer">Яна, видеограф, Франция</p>
               </div>
               <div class="reviews__slider__slide">
-                <img src="./assets/image/reviews/Tatyana_Shv.jpeg" alt="отзывы" />
+                <img src="./assets/image/reviews/Tatyana_Shv.jpeg" alt="отзывы" class="vertikal" />
                 <p class="reviewer">Татьяна, художница, Швейцария</p>
               </div>
               <div class="reviews__slider__slide">
-                <img src="./assets/image/reviews/Alena_rus.jpeg" alt="отзывы" />
+                <img src="./assets/image/reviews/Alena_rus.jpeg" alt="отзывы" class="gorozont" />
                 <p class="reviewer">Алёна, преподаватель английского языка, Россия</p>
               </div>
             </div>
           </div>
           <div class="reviews__slider__button-right">
-            <img src="./assets/image/reviews/arrow.svg" alt="arrow_next" id="next" />
+            <!-- <img src="./assets/image/reviews/arrow.svg" alt="arrow_next" id="next" /> -->
+            <svg width="40" height="40" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" alt="arrow_next" id="next" class="arrow">
+              <circle cx="15" cy="15" r="14.1" stroke="#557DB0" stroke-width="1.8" />
+              <path d="M20.5 8L7 14.5L20.5 22.5" stroke="#557DB0" stroke-width="1.8" />
+            </svg>
           </div>
         </div>
       </div>
