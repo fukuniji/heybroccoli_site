dfc29cf825368771c4d27d3d98b5aee67ba095eb
Evgenia0712 <e.gunko1992@mail.ru>
2023-05-31 00:09:26 +0300

    feat: позиционирует 7 блок

--- services-business.php
+++ services-business.php
@@ -1,18 +1,18 @@
   <!-- ---------------------SERVICES-FOR-BUSINESS-7-------------------------->
 
-  <div class="main__services-business services-business">
+  <div class="main__services-business">
     <div class="container">
 
-      <div class="services-business__business">
+      <div class="services-business">
         <h2 class="services-business__title title">Услуги для бизнеса </h2>
-        <div class="services-business__info-wrapper">
+        <!--<div class="services-business__info-wrapper">-->
           <img class="services-business__img-1" src="./assets/image/services-business/2people.png" alt="Елена с коллегой">
           <div class="services-business__text-1"> Если вы <span class="bluetext"> подрядчик</span>, который способен организовать вкусный и сбалансированный <span class="bluetext "> кейтеринг</span>, тоже напишите мне, найдем возможности для сотрудничества.</div>
           <div class="services-business__text-2">Если вы хотите придумать классные и полезные закуски на <span class="bluetext ">вечеринку, детский праздник, пресс-конференцию</span>, танцевальный <span class="bluetext ">фестиваль</span> или провести <span class="bluetext ">лекцию</span> для своих сотрудников по теме питания, подобрать меню для столовой или ретрита — я могу вам помочь.</div>
           <div class="services-business__img-2">
             <img class="img-2" src="./assets/image/services-business/4people.png" alt="Елена с коллегами">
           </div>
-        </div>
+        <!--</div>-->
       </div>
 
     </div>
