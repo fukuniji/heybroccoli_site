7dc77e5fafb73a5b125e53558feeafff00ce3f66
Lanasites <mochegova@gmail.com>
2023-06-01 02:16:11 +0300

    fix: правки по высоте строчек мелкого текста и отступам

--- sertificates.php
+++ sertificates.php
@@ -5,28 +5,28 @@
       <h2 class="sertificates__title title"> Мои сертификаты, обучение </h2>
       <div class="sertificates__block block ">
         <div class="block__text_1">
-          <span class="bluetext ">Основа моего подхода</span> — знания и навыки, полученные в канадской Precision Nutrition. Главная особенность PN — сочетание научных знаний и индивидуального подхода. Этим нутрициолог отличается от диетолога: второй говорит, что нужно делать. Первый — что и как.
+          <span class="bluetext text">Основа моего подхода</span> — знания и навыки, полученные в канадской Precision Nutrition. Главная особенность PN — сочетание научных знаний и индивидуального подхода. Этим нутрициолог отличается от диетолога: второй говорит, что нужно делать. Первый — что и как.
         </div>
-        <div class="block__photo_1">
+        <div class="block__photo_1 text">
           <img src="./assets/image/sertificates/sertificate-1.jpg" alt="Сертификат">
           Квинтэссенция знаний о том, как иммунитет зависит от еды (а не от БАДов)
         </div>
-        <div class="block__photo_2">
+        <div class="block__photo_2 text">
           <img src="./assets/image/sertificates/sertificate-2.jpg" alt="Сертификат">
           Это был настоящий марафон с консультациями под присмотром более опытных коллег, мега-полезно и эффективно
         </div>
         <div class="block__line">
           <img src="./assets/image/sertificates/line_10.svg" alt="Линия">
         </div>
-        <div class="block__photo_3">
+        <div class="block__photo_3 text">
           <img src="./assets/image/sertificates/sertificate-3.jpg" alt="Сертификат">
           Моя самая главная учеба, Precision Nutrition. Я доучиваюсь, но уже могу проводить консультации, рамки компетенций четко понимаю
         </div>
-        <div class="block__photo_4">
+        <div class="block__photo_4 text">
           <img src="./assets/image/sertificates/sertificate-4.jpg" alt="Сертификат">
           Обучение по управлению стрессом и восстановлению. Очень желанный курс, начинаю потихоньку, пока не практикую отдельно
         </div>
-        <div class="block__photo_5">
+        <div class="block__photo_5 text">
           <img src="./assets/image/sertificates/sertificate-5.jpg" alt="Оценки">
           Пример моих оценок на экзаменах. Я ботан!
         </div>
--- task.php
+++ task.php
@@ -4,21 +4,21 @@
     <div class="container">
       <h2 class="task__title title">С какими задачами я могу помочь </h2>
       <div class="task__row row">
-        <div class="row__text"><i class="icon icon-1"></i>надоело есть одно и то же</div>
-        <div class="row__text"><i class="icon icon-2"></i>постоянно ем, но не наедаюсь</div>
-        <div class="row__text"><i class="icon icon-3"></i>хочу перестать есть сладкое</div>
-        <div class="row__text"><i class="icon icon-4"></i>хочу научиться есть овощи</div>
-        <div class="row__text"><i class="icon icon-5"></i>я не ем мясо, где брать белок</div>
-        <div class="row__text"><i class="icon icon-6"></i>
+        <div class="row__text text"><i class="icon icon-1"></i>надоело есть одно и то же</div>
+        <div class="row__text text"><i class="icon icon-2"></i>постоянно ем, но не наедаюсь</div>
+        <div class="row__text text"><i class="icon icon-3"></i>хочу перестать есть сладкое</div>
+        <div class="row__text text"><i class="icon icon-4"></i>хочу научиться есть овощи</div>
+        <div class="row__text text"><i class="icon icon-5"></i>я не ем мясо, где брать белок</div>
+        <div class="row__text text"><i class="icon icon-6"></i>
           я переехал(а) в новую страну и не понимаю, что есть
         </div>
-        <div class="row__text"><i class="icon icon-7"></i>
+        <div class="row__text text"><i class="icon icon-7"></i>
           хочу питаться разнообразно и не проводить жизнь на кухне
         </div>
-        <div class="row__text"><i class="icon icon-8"></i>
+        <div class="row__text text"><i class="icon icon-8"></i>
           я не чувствую голод и ем потому, что надо
         </div>
-        <div class="row__text"><i class="icon icon-9"></i>
+        <div class="row__text text"><i class="icon icon-9"></i>
           у меня жесткий график, я не успеваю нормально есть
         </div>
       </div>
